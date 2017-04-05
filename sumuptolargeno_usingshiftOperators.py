2865a6246ec4fc11f006fba5aabac4224e157a22
trigodeepak <trigodeepak@gmail.com>
2017-04-05 21:51:37 +0530

    Create sumuptolargeno_usingshiftOperators.py

---
+++ sumuptolargeno_usingshiftOperators.py
@@ -0,0 +1,13 @@
+#sumuptolargeno_usingshiftOperators.py
+a = raw_input().strip()
+#print a
+a = int(a,2)
+print a
+b = raw_input().strip()
+#print b,type(b)
+b = int(b,2)
+print b<<2
+s=0
+for i in range(314159):
+    s+=a^(b<<(i))
+print s%(10**9+7)
