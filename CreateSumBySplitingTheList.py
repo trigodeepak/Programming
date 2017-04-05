0bffc2388592f1d8a0738ae69954ab4e7554cf66
trigodeepak <trigodeepak@gmail.com>
2017-04-05 22:20:03 +0530

    Create CreateSumBySplitingTheList.py

---
+++ CreateSumBySplitingTheList.py
@@ -0,0 +1,17 @@
+n = input()
+a = map(int,raw_input().strip().split(' '))
+b=[0 for i in range(n)]
+#to create pieces of the array
+b[0]= a[0]
+def createSum(a,p,i):
+    s=0
+    if(p<i):
+        for j in range(p,i):
+            s+=createSum(a,j,i)
+        return s
+    else:
+        return a[i]
+for i in range(1,n):
+    b[i]=(a[i-1]+createSum(a,0,i))
+print b   
+    
