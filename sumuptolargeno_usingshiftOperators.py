4bc1ea98010738f71428d9f3b05f2b4aaf9283f4
trigodeepak <trigodeepak@gmail.com>
2017-04-05 22:22:40 +0530

    Update sumuptolargeno_usingshiftOperators.py

--- sumuptolargeno_usingshiftOperators.py
+++ sumuptolargeno_usingshiftOperators.py
@@ -11,3 +11,4 @@ s=0
 for i in range(314159):
     s+=a^(b<<(i))
 print s%(10**9+7)
+#Done in python for large numbers but they say large numbers not needed, output is not correct after using python though
