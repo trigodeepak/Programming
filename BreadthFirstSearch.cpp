7f9f5cb3f3b9ee0556fff14a5f883c55ef442eea
Deepak Yadav <trigodeepak@gmail.com>
2017-04-14 22:52:51 +0530

    Create BreadthFirstSearch.cpp

---
+++ BreadthFirstSearch.cpp
@@ -0,0 +1,50 @@
+//Program for bfs in graph NOT working correctly and providing incorrect or correct I cant determine
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+static int time = 0;
+int inf = 99999;
+int par[5],d[5];
+char colour[5][1];
+int que[5];
+int k=0;int f=0;
+void bfs(int a[][5],int n,int s)
+{   int i,j;
+    for (i=0;i<n;i++){
+    if(i==s)
+        continue;
+    colour[5][0]='W';
+    d[i]=inf;
+    par[i]=-1;
+    }
+    colour[s][0]='G';
+    d[s]=0;
+    par[s]=-1;
+    que[k++]=s;
+    while(k>=f){
+        int u = que[f++];
+        printf("%d ",u);
+        for(j=0;j<n;j++){
+                if(j==u)
+                    continue;
+		if (a[u][j]==1){
+			if(colour[j][0]=='W'){
+				colour[j][0]='G';
+                d[j]=d[u]+1;
+                par[j]=u;
+                que[k++]=j;
+                }
+            }
+        }
+	colour[u][0]='B';
+    }
+
+}
+int main()
+{
+//      FILE *fp;
+      int n=5;
+      int a[5][5]= {{0,1,0,0,1},{1,0,1,1,0},{0,1,0,0,1},{0,1,1,0,1},{1,1,0,1,0}};
+	  bfs(a,n,4);
+return 0;
+}
