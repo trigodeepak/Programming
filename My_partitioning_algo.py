2a38abeeffb419c5927d2fc67223858b06dbc3f1
Deepak Yadav <trigodeepak@gmail.com>
2017-04-18 23:00:00 +0530

    Update My_partitioning_algo.py

--- My_partitioning_algo.py
+++ My_partitioning_algo.py
@@ -1,37 +1,26 @@
 # Partition Problem
 a = [1,5,11,5]
 n = len(a)
-a.sort()
+##a.sort()
 s = sum(a)
 b=[]
 c=[]
 f=0
-#This algorithm does that it makes two list from beginning and end and keep
-#on comparing their sum if same and we have traversed the whole array then
-#The output will be yes in all other cases the output will be no.
-for i in range(n):
-  if s % 2 != 0:
-    f = 0
-    break
-  k = 0
-  c.append(a[k])
-  j = n-1
-  b.append(a[j])
-  f=1
-  while sum(b)!=sum(c) and (j + k <= n):
-    if(sum(b)<sum(c)):
-      b.append(a[j-1])
-      j-=1
-      print b
-    else:
-      c.append(a[k+1])
-      k+=1
-      print c
-    if(k>=j):
-      f=0
-      break
-if f==0:
+#Now I have modified using geeksfor geeks approach
+if s % 2 == 0:
+    part=[[True for i in range(n+1)]for i in range(s/2+1)]
+    for i in range(n):
+        part[0][i]=True
+    for i in range(1,s/2):
+        part[i][0]=False
+    for i in range(1,s/2):
+        for j in range(1,n):
+            part[i][j]=part[i][j-1]
+            if i > a[j-1]:
+                part[i][j]=part[i][j] or part[i - a[j-1]][j-1]
+    f = part[s/2][n]
+  
+if f==False:
   print "NO"
 else:
   print "YES"
-#At last it worked for the sample test case ; )
