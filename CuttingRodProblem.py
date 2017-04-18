120a891316141d644ae906674188c78319d156c5
Deepak Yadav <trigodeepak@gmail.com>
2017-04-18 22:23:56 +0530

    Update CuttingRodProblem.py

--- CuttingRodProblem.py
+++ CuttingRodProblem.py
@@ -1,15 +1,13 @@
 #Program for Cutting rod problem
 ##a = map(int, raw_input().strip().split(' '))
-#Not working correctly for some examples
+
 p = [1, 5, 8, 9, 10, 17, 17, 20]
 n = len(p)
-c = [[0 for i in range(n+2)]for i in range(n)]
-for i in range(n+2):
-    c[0][i] = p[0]*i
-for i in range(1,n):
-    for j in range(n+2):
-        if(j>i):
-            c[i][j]=max(c[i-1][j],p[i]+c[i][j-i-1])
-        else:
-            c[i][j]=c[i-1][j]
-print c
+c = [0 for i in range(n+1)]
+for i in range(1,n+1):
+    max_value=-1
+    for j in range(i):
+        c[i]= max(max_value,p[j]+c[i-j-1])
+        max_value = c[i]
+        
+print c[n]
