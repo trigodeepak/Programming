90ecf1f7820bb2ddb578a16d98aab5172de9c4ce
Deepak Yadav <trigodeepak@gmail.com>
2017-04-21 08:07:51 +0530

    Create BellmanFord.cpp

---
+++ BellmanFord.cpp
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <iostream>
+using namespace std;
+
+// a structure to represent a weighted edge in graph
+struct Edge
+{
+    int src, dest, weight;
+};
+
+
+// a structure to represent a connected, directed and
+// weighted graph
+struct Graph
+{
+    // V-> Number of vertices, E-> Number of edges
+    int V, E;
+
+    // graph is represented as an array of edges.
+    struct Edge* edge;
+};
+
+// Creates a graph with V vertices and E edges
+struct Graph* createGraph(int V, int E)
+{
+    struct Graph* graph =
+         (struct Graph*) malloc( sizeof(struct Graph) );
+    graph->V = V;
+    graph->E = E;
+
+    graph->edge =
+       (struct Edge*) malloc( graph->E * sizeof( struct Edge ) );
+
+    return graph;
+}
+void relax(Edge e,int *d)
+{
+    if((d[e.dest])>(d[e.src]+e.weight)){
+        (d[e.dest])=(d[e.src]+e.weight);
+    }
+}
+void BellmanFord(Graph *g,int src){
+    int V = g->V;
+    int E = g->E;
+    //Initialize distance infinite
+    int i,dist[V];
+    for(i=0;i<V;i++)
+        dist[i] = INT_MAX;
+    dist[src] = 0;
+    for(i=0;i<E;i++){
+        relax(g->edge[i],dist);
+    }
+
+    //code for negative cycle exist
+    for(i=0;i<E;i++){
+        Edge e=g->edge[i];
+        if((d[e.dest])>(d[e.src]+e.weight)){
+        (d[e.dest])=(d[e.src]+e.weight);
+        cout<<"The Graph have a negative cycle";
+        return;
+    }
+    }
+    cout<<"Vertex\tDistance from source\n";
+    for(i=0;i<V;i++){
+        cout<<i<<" \t  "<<dist[i]<<"\n";
+    }
+    cout<<"done";
+
+
+
+}
+
+int main()
+{
+    /* Let us create the graph given in above example */
+    int V = 5;  // Number of vertices in graph
+    int E = 8;  // Number of edges in graph
+    struct Graph* graph = createGraph(V, E);
+
+    // add edge 0-1 (or A-B in above figure)
+    graph->edge[0].src = 0;
+    graph->edge[0].dest = 1;
+    graph->edge[0].weight = -1;
+
+    // add edge 0-2 (or A-C in above figure)
+    graph->edge[1].src = 0;
+    graph->edge[1].dest = 2;
+    graph->edge[1].weight = 4;
+
+    // add edge 1-2 (or B-C in above figure)
+    graph->edge[2].src = 1;
+    graph->edge[2].dest = 2;
+    graph->edge[2].weight = 3;
+
+    // add edge 1-3 (or B-D in above figure)
+    graph->edge[3].src = 1;
+    graph->edge[3].dest = 3;
+    graph->edge[3].weight = 2;
+
+    // add edge 1-4 (or A-E in above figure)
+    graph->edge[4].src = 1;
+    graph->edge[4].dest = 4;
+    graph->edge[4].weight = 2;
+
+    // add edge 3-2 (or D-C in above figure)
+    graph->edge[5].src = 3;
+    graph->edge[5].dest = 2;
+    graph->edge[5].weight = 5;
+
+    // add edge 3-1 (or D-B in above figure)
+    graph->edge[6].src = 3;
+    graph->edge[6].dest = 1;
+    graph->edge[6].weight = 1;
+
+    // add edge 4-3 (or E-D in above figure)
+    graph->edge[7].src = 4;
+    graph->edge[7].dest = 3;
+    graph->edge[7].weight = -3;
+
+    BellmanFord(graph, 0);
+
+    return 0;
+}
