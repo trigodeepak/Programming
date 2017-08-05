ff0ac0a56e4464150899a8d793981866b2ff133c
Deepak Yadav <trigodeepak@gmail.com>
2017-08-05 12:15:06 +0530

    Create stock_span_problem.py

---
+++ stock_span_problem.py
@@ -0,0 +1,14 @@
+#Program for stock span problem
+a= [100,80,60,70,60,75,85]
+n = len(a)
+max_till = 1
+c = [1 for i in xrange(n)]
+for i in xrange(n):
+    for j in xrange(i):
+        if(a[i]>a[j]):
+            max_till += 1
+        else:
+            max_till = 1
+    c[i] = max_till
+print c
+# The answer is an array showing max span for each stock day.
