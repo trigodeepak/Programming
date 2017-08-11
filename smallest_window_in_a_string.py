c187bef7ace4257dd98ea667c2d619678675f11c
Deepak Yadav <trigodeepak@gmail.com>
2017-08-11 20:33:53 +0530

    Create smallest_window_in_a_string.py

---
+++ smallest_window_in_a_string.py
@@ -0,0 +1,31 @@
+#Program for the smallest window
+a = 'abmmebezedeedzbfjfazdjdje'
+a = list(a)
+l = len(a)
+b = 'ebdz'
+b = list(b)
+n = len(b)
+c = {}
+for i in xrange(n):
+    c[b[i]] = 0
+count = 0
+window = float('inf')
+prev = -444
+for i in xrange(l):
+    if (a[i] in b):
+        char = a[i]
+        if(c[a[i]]== 0 ):
+            count+=1
+        prev = c[a[i]]
+        c[a[i]] = i
+        if count == n:
+            val = max(c.values())-min(c.values())
+            if (window < val):
+                c[a[i]] = prev
+            else:
+                window = val
+##print window + 1
+mini = min(c.values())
+maxi = max(c.values())
+for i in xrange(mini,maxi+1):
+    print a[i],
