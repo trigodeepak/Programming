acf2e19e9d5eb43ffcb790fb69b81bf31171e827
Deepak Yadav <trigodeepak@gmail.com>
2017-10-21 18:17:27 +0530

    Create Sort_according_frequency.py

---
+++ Sort_according_frequency.py
@@ -0,0 +1,15 @@
+#program to sort an array according to frequency
+a = [2, 3, 2, 4, 5, 12, 2, 3, 3, 3, 12]
+#creating dictionary 
+dict = {}
+for i in a:
+    if dict.has_key(i):
+        dict[i]+=1
+    else:
+        dict[i]=1
+b = []
+for i in dict:
+    b.append((i,dict[i]))
+b = sorted(b,key=lambda x: x[1],reverse=True)
+#the array is not sorted
+
