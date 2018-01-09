1a5a35e46bffbe9ef4e2ad95845135adc9d7f805
Deepak Yadav <trigodeepak@gmail.com>
2018-01-09 22:15:59 +0530

    Update Palindrome_partioning.py

--- Palindrome_partioning.py
+++ Palindrome_partioning.py
@@ -1,44 +1,40 @@
-###Palindrome partitioning
-##n = input()
-##s = map(int,raw_input().strip().split(' '))
-##a = [1000 for i in range(n)]
-##b = [-1 for i in range(n)]
-##a[0]=0
-##a[1]=1
-##print s
-##for  i in range(2,n):
-##    for j in range(0,i-1):
-##        if((s[j]+j)>=i):
-##            print i,j
-##            a[i]=min(a[j]+1,a[i])
-##            print a
-##            #b[i]=j
-##print a
-##print b
-### The program is not working fine but have done the logic properly
-
-
-# I don't know why it wasn't working this is a better approach palindrome partitioning
-a = "abcbm"
-l = len(a)
-c = [[0 for i in xrange(l)]for x in xrange(l)]
-
-#This loop is to access the matrix diagonally from left to right
-for k in xrange(l):
-    i = 0
-    j = k
-    while(i<l-k):
-        if i == j:
-            c[i][j] = 0
-        elif (a[i] == a[j]):
-            c[i][j] = 0
-        else:
-            min_num = c[i][0]+c[1][j]
-            for x in xrange(i,j-1):
-                min_num = min(min_num,c[i][x]+c[x+1][j])
-            c[i][j] = 1 + min_num
-        i+=1
-        j+=1
-for i in c:
-    print i
 #Program for palindrome partitioning
+def minPalPartition(string):
+    length = len(string)
+    dp_cuts = [[0 for i in range(length)]for x in range(length)]
+    #This matrix is to determine that the given substring is palindrome or not
+    p = [[False for i in range(length)]for x in range(length)]
+    #This loop is to access the matrix diagonally from left to right
+    for k in range(length):
+        i = 0
+        j = k
+        while(i<length-k):
+            #First 3 conditions are to check if substring is palindrome
+            if i == j:
+                p[i][j] = True
+                dp_cuts[i][j] = 0
+            elif k+1 == 2:
+                p[i][j] = (string[i] == string[j])
+            else:
+                p[i][j] = (string[i] == string[j]) and p[i+1][j-1]
+            #Following conditions are to find the minimum cut  
+            if (p[i][j]==True):
+                dp_cuts[i][j] = 0
+            else:
+                dp_cuts[i][j] = float('inf')
+                #Make a cut at every possible location and get the minimum cost
+                for x in range(i,j):
+                    dp_cuts[i][j] = min(dp_cuts[i][j],dp_cuts[i][x]+dp_cuts[x+1][j]+1)
+            i+=1
+            j+=1
+    #Uncomment below lines to print the matrix obtained
+##    for row in dp_cuts:
+##        print row
+
+    #returning the minimum cut value
+    return dp_cuts[0][length-1]
+
+#Driver program
+string = "ababbbabbababa"#raw_input().strip() #to take user input
+print "Minimum cuts to form a palindrome is :",minPalPartition(string)
+
