2a0bf784f09bd99fcac24679278c4611a6b749d9
Deepak Yadav <trigodeepak@gmail.com>
2018-05-11 09:39:02 +0530

    Update smallest_window_in_a_string.py

--- smallest_window_in_a_string.py
+++ smallest_window_in_a_string.py
@@ -14,28 +14,27 @@ b = []
 for i in xrange(n):
     d = raw_input().strip()
     b.append(d)
-    
+
+#C is count dictionary
 c = {}
 for i in xrange(n):
     c[b[i]] = 0
 count = 0
 window = float('inf')
-prev = -444
 for i in xrange(l):
     if (a[i] in b):
         char = a[i]
         if(c[a[i]]== 0 ):
             count+=1
-        prev = c[a[i]]
         c[a[i]] = i
         if count == n:
             val = max(c.values())-min(c.values())
-            if (window < val):
-                c[a[i]] = prev
-            else:
+            if window > val:
                 window = val
+#To print the window size
 ##print window + 1
 mini = min(c.values())
 maxi = max(c.values())
 for i in xrange(mini,maxi+1):
     print a[i],
+    
