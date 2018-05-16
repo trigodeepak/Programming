a1aa7147326c81a45984bff94644b532b67d5ee9
Deepak Yadav <trigodeepak@gmail.com>
2018-05-16 10:28:08 +0530

    Create median in a stream.py

---
+++ median in a stream.py
@@ -0,0 +1,39 @@
+#Median in a stream using 2 heaps the program is running for most of the test cases
+#will figure out the error from another prespective later
+def rebalance(h_min,h_max,l1,l2):
+    if l1>l2:
+        heapq.heappush(h_max,-heapq.heappop(h_min))
+        l1-=1;l2+=1
+    else:
+        heapq.heappush(h_min,-heapq.heappop(h_max))
+        l2-=1;l1+=1
+    return l1,l2
+
+import heapq
+h_min = []
+h_max = []
+l1 = 0
+l2 = 0
+med = 0
+t = int(input())
+for i in range(t):
+    n = int(input())
+##    a = list(map(int,input().split()))
+    if l1 == 0 or n<med:
+        heapq.heappush(h_min,-n)
+        l1+=1
+    elif n>med:
+        heapq.heappush(h_max,n)
+        l2+=1
+    if abs(l1-l2) >= 2:
+        l1,l2 = rebalance(h_min,h_max,l1,l2)
+        
+    #finding median
+    if l1>l2:
+        med = -h_min[0]
+    elif l1<l2:
+        med = h_max[0]
+    else:
+        med = ((-h_min[0]+h_max[0])/2.0) 
+    print (med)
+    
